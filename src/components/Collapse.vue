51703de9f70ff7dc3193c734be3a9d41c88e9df4
Kararakt <xom-vlaskin0@outlook.com>
2022-11-27 15:19:10 +0300

    git update

--- src/components/Collapse.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-const visible = ref(false);
-</script>
-<template>
-  <div @click="visible = !visible"><slot name="header"></slot></div>
-  <div v-if="visible"><slot></slot></div>
-</template>
